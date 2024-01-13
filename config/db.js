7cf4a3da2c1a3bbeed2fec3af60be5f4f733c1b2
Manoj <manojgowdacs35@gmail.com>
2024-01-13 22:47:12 +0530

    bug fixes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import colors from 'colors'
 const connectDB = async ()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Connected to MongoDB ${conn.connection.host}`.bgGreen.white);
+        console.log(`Connected to MongoDB ${conn.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`Error in the Database ${error}`.bgRed.white);
     }
