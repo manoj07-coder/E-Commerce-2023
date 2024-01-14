9c44c3de36418912b3692bd9e4385f2604296982
Manoj <manojgowdacs35@gmail.com>
2024-01-14 18:51:12 +0530

    register user functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,15 @@
+import { hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
 
 
-export const registerController = async(req,res)=>{
-    
+export const registerController = async(req,res,next)=>{
+    const {name,email,password,phone,address} = req.body;
+    const hashedPassword = await hashPassword(password);
+    const newUser = new userModel({name,email,phone,address,password:hashedPassword});
+    try {
+        await newUser.save();
+        res.status(201).json('User created succesfully');
+    } catch (error) {
+        next(error)
+    }
 }
