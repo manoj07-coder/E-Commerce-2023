525cddf433523fcbeb75e87f518dbec45a67257c
Manoj <manojgowdacs35@gmail.com>
2024-01-14 22:27:31 +0530

    login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import { errorHandler } from "../helpers/errorHandler.js";
 import userModel from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
+
 
 
 export const registerController = async(req,res,next)=>{
@@ -9,6 +12,27 @@ export const registerController = async(req,res,next)=>{
     try {
         await newUser.save();
         res.status(201).json('User created succesfully');
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const loginController = async(req,res,next) =>{
+    const {email,password} = req.body;
+    try {
+        const validUser = await userModel.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,'User not found'))
+        }
+        const validPassword = await comparePassword(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Wrong credentials'));
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:pass,...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200).json(rest);
+        
     } catch (error) {
         next(error)
     }
