b33e4f1307166c12d977b456ea4c712ec476e80f
Manoj <manojgowdacs35@gmail.com>
2024-01-14 18:50:44 +0530

    added error middleware

--- index.js
+++ index.js
@@ -24,3 +24,13 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server listening on port ${port}`.bgBlue);
 })
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
